ff62f8a34b38ab87ff24c01807bfb228b63e9ab8
ToniPanovTMS <-l>
2023-08-14 20:02:05 +0300

    /

--- src/GUI15.java
+++ src/GUI15.java
@@ -5,9 +5,53 @@ import java.awt.*;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
 
-class GUI15 {
+class GUI15 extends JFrame {
     static int count=0;
+
     public static void main(String[] args) {
+        JFrame jFrame=getFrame();
+        JPanel jPanel= new JPanel();
+        jFrame.add(jPanel);
+
+        SpinnerModel value =
+                new SpinnerNumberModel(0,
+                        0,
+                        100,
+                        1);
+        JSpinner jSpinner = new JSpinner(value);
+        jSpinner.setBounds(50,50,50,30);
+        jPanel.add(jSpinner);
+        jSpinner.addChangeListener(new ChangeListener() {
+            @Override
+            public void stateChanged(ChangeEvent e) {
+                count = (int) jSpinner.getValue();
+            }
+        });
+
+        JButton jButton = new JButton("Ответить");
+
+        jButton.setBounds(150,40,100,50);
+        jPanel.add(jButton);
+
+        JLabel jLabel = new JLabel("Ответ: "+count);
+        jLabel.setLocation(260,40);
+        jPanel.add(jLabel);
+
+        jButton.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                jLabel.setText("Ответ: "+count);
+            }
+        });
+
+
+
+        jFrame.setLayout(null);
+        jFrame.setVisible(true);
+
+    }
+
+/*    public static void main(String[] args) {
         JFrame jFrame=getFrame();
         JPanel jPanel =new JPanel();
         jFrame.add(jPanel,BorderLayout.SOUTH);
@@ -16,21 +60,23 @@ class GUI15 {
         jFrame.setLocationRelativeTo(null);
         JLabel jLabel= new JLabel("Колличество нажатий: "+count);
         jButton.addActionListener(new ActionListener() {
-            @Override
-            public void actionPerformed(ActionEvent e) {
-                count++;
+          @Override
+          public void actionPerformed(ActionEvent e) {
+               count++;
                 jLabel.setText("Колличество нажатий: "+count);
             }
         });
         jPanel.add(jLabel);
     }
-    static JFrame getFrame(){
+
+ */
+   static JFrame getFrame(){
         JFrame jFrame= new JFrame(){};
         jFrame.setTitle("GUI14");
-        jFrame.setVisible(true);
         Dimension dim = Toolkit.getDefaultToolkit().getScreenSize();
-        jFrame.setBounds(dim.width / 2 - 700 / 2, dim.height / 2 - 300 / 2, 700, 300);
+        jFrame.setBounds(dim.width / 2 - 400 / 2, dim.height / 2 - 150 / 2, 400, 150);
         jFrame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+       jFrame.setVisible(true);
         return jFrame;
     }
 }
